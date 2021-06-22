4cd5526270251494c4871c2220e05f76caf2bf3c
Hrittik Roy <67012359+hrittikhere@users.noreply.github.com>
2021-06-22 18:47:29 +0530

    Create 1_name_print.cpp

---
+++ 1_name_print.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+	int a;
+	cin >> a;
+	string temp, name;
+	getline(cin, temp);
+	while (a >= 1)
+	{
+		getline(cin, name);
+		cout << "Hi it's " << name << "! \n";
+		a -= 1;
+	}
+}
+
+signed main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+#ifndef x
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	//int _t:cin>>_t:while(_t--)
+	solve();
+}
